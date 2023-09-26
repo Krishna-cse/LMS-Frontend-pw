a00348dde77c4f06690ed82bf17d1094cf02bbf2
Krishna-cse <krishnaofficialemail@gmail.com>
2023-09-26 16:54:52 +0530

    Adding basic redux setup with toolkit

--- src/main.jsx
+++ src/main.jsx
@@ -4,15 +4,19 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import {BrowserRouter} from 'react-router-dom'
 import { Toaster } from 'react-hot-toast'
+import {Provider} from 'react-redux'
 
 import App from './App.jsx'
+import store from './Redux/Store'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   //<React.StrictMode>
-  <BrowserRouter>
-    <App />
-    <Toaster/>
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+      <Toaster/>
+    </BrowserRouter>
+  </Provider>
     
  // </React.StrictMode>,
 )
